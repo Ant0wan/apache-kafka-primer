c306e279d3bd3406463f85a670aa53e9e26727ff
Antoine Barthelemy <antoine@linux.com>
2023-04-10 20:28:33 +0200

    test

--- setup.sh
+++ setup.sh
@@ -14,6 +14,8 @@ install -d -m 0755 -o "$USER" -g "$GROUP" "$WORKDIR"
 curl https://dlcdn.apache.org/kafka/${VERSION}/kafka_2.13-${VERSION}.tgz --output ${WORKDIR}/kafka_2.13-${VERSION}.tgz
 tar -xzf ${WORKDIR}/kafka_2.13-${VERSION}.tgz
 rm -rf ${WORKDIR}/kafka_2.13-${VERSION}.tgz
+
 pushd ${WORKDIR}/kafka_2.13-${VERSION}/ || exit 1
 export PATH=$PATH:/opt/java/jre1.8.0_361/bin/
 popd || exit 1
+
