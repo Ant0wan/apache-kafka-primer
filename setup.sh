a579f87beb0df5f3cc01e00a8098e07d7a37d4f0
Antoine Barthelemy <antoine@linux.com>
2023-04-10 20:57:43 +0200

    test

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ sudo install -d -m 0755 -o "$USER" -g "$GROUP" "$WORKDIR"
 
 curl https://dlcdn.apache.org/kafka/${VERSION}/kafka_2.13-${VERSION}.tgz --output kafka_2.13-${VERSION}.tgz
 tar -xzf kafka_2.13-${VERSION}.tgz
-sudo mv kafka_2.13-${VERSION} ${WORKDIR}
+sudo mv kafka_2.13-${VERSION}/* ${WORKDIR}
 sudo chown -R $USER:$GROUP ${WORKDIR}
-rm -rf kafka_2.13-${VERSION}.tgz
+rm -rf kafka_2.13-${VERSION}.tgz kafka_2.13-${VERSION}
 
 # Not needed
 sudo runuser -l "$USER" -c "
