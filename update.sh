5aa2e2c586a1bdaab34d40a768da4d41efca91dc
Antoine Barthelemy <antoine@linux.com>
2023-06-06 18:07:55 +0200

    update provider

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-providers="$(hclq get terraform.required_providers.*.source --in main.tf | awk '{ gsub(/[\[\]"]/, ""); gsub(/,/, " "); print }')"
-versions="$(hclq get terraform.required_providers.*.version --in main.tf)"
+files="$(grep required_providers -nR | awk -F ':' '{ print $1}' | uniq | grep *.tf)"
 
-for each in $providers
+for file in $files
 do
-	provider="$(echo $each | awk -F '/' '{print $2}')"
-	latest="$(curl "https://registry.terraform.io/v1/providers/${each}" 2>/dev/null | jq .version -r)"
-	hclq set terraform.required_providers.${provider}.version "${latest}" --in main.tf --out main.tf
+	providers="$(hclq get terraform.required_providers.*.source --in ${file} | awk '{ gsub(/[\[\]"]/, ""); gsub(/,/, " "); print }')"
+	for each in $providers
+	do
+		provider="$(echo $each | awk -F '/' '{print $2}')"
+		latest="$(curl "https://registry.terraform.io/v1/providers/${each}" 2>/dev/null | jq .version -r)"
+		echo "${provider} = ${latest}"
+		hclq set terraform.required_providers.${provider}.version "${latest}" --in ${file} --out ${file}
+	done
 done
