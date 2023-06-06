ed32c5f7e25d9634baa228d43c24bee804e4aad3
Antoine Barthelemy <antoine@linux.com>
2023-06-06 18:11:51 +0200

    update provider

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-files="$(grep required_providers -nR | awk -F ':' '{ print $1}' | uniq | grep *.tf)"
+files="$(grep required_providers -nR | awk -F ':' '{ print $1}' | uniq | grep -E '.[.]tf')"
 
 for file in $files
 do
-	providers="$(hclq get terraform.required_providers.*.source --in ${file} | awk '{ gsub(/[\[\]"]/, ""); gsub(/,/, " "); print }')"
+	providers="$(hclq get terraform.required_providers.*.source --in "${file}" | awk '{ gsub(/[\[\]"]/, ""); gsub(/,/, " "); print }')"
 	for each in $providers
 	do
-		provider="$(echo $each | awk -F '/' '{print $2}')"
+		provider="$(echo "$each" | awk -F '/' '{print $2}')"
 		latest="$(curl "https://registry.terraform.io/v1/providers/${each}" 2>/dev/null | jq .version -r)"
 		echo "${provider} = ${latest}"
-		hclq set terraform.required_providers.${provider}.version "${latest}" --in ${file} --out ${file}
+		hclq set terraform.required_providers."${provider}".version "${latest}" --in "${file}" --out "${file}"
 	done
 done
